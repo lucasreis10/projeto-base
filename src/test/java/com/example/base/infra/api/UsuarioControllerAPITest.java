625d623f2c9e724223826962f478c8788d2cc3e1
Lucas Reis <lucaschristian10@gmail.com>
2023-01-15 22:22:40 -0300

    test(usuario): criar teste para verificar fluxo de execao

--- src/test/java/com/example/base/infra/api/UsuarioControllerAPITest.java
+++ src/test/java/com/example/base/infra/api/UsuarioControllerAPITest.java
@@ -3,6 +3,7 @@ package com.example.base.infra.api;
 import com.example.base.application.usuario.criar.CriarUsuarioOutput;
 import com.example.base.application.usuario.criar.CriarUsuarioUseCase;
 import com.example.base.application.usuario.login.LoginUsuarioUseCase;
+import com.example.base.domain.exception.DomainException;
 import com.example.base.infra.ControllerTest;
 import com.example.base.infra.usuario.models.CriarUsuarioAPIInput;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -67,6 +68,43 @@ public class UsuarioControllerAPITest {
                 .andExpect(MockMvcResultMatchers.header().string("Location",  "/usuarios/dummy-id"))
                 .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(jsonPath("$.id", Matchers.equalTo("dummy-id")));
+
+        Mockito.verify(criarUsuarioUseCase, times(1)).execute(Mockito.argThat(cmd ->
+                Objects.equals(nomeEsperado, cmd.getNome())
+                && Objects.equals(senhaEsperada, cmd.getSenha())
+                && Objects.equals(emailEsperado, cmd.getEmail())
+        ));
+
+    }
+
+    @Test
+    public void dadoUmNomeNullInvalido_quandoExecutarCriarUsuario_deveSerRetornadoNotificationErroStatus422() throws Exception{
+         final var emailEsperado = "gleidisney@email.com.br";
+        final String nomeEsperado = null;
+        final var senhaEsperada = "osi03w3";
+        final var mensagemErroEsperada = "'nome' não pode ser nulo.";
+
+        final var input =
+                new CriarUsuarioAPIInput(nomeEsperado, senhaEsperada, emailEsperado);
+
+        when(criarUsuarioUseCase.execute(any()))
+                .thenThrow(new DomainException(mensagemErroEsperada));
+
+        // execute:
+        final var request = post("/usuarios")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(input));
+
+        final var response = mvc.perform(request)
+                .andDo(MockMvcResultHandlers.print());
+
+        // verify:
+        response
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isUnprocessableEntity())
+                .andExpect(header().string("Location", Matchers.nullValue()))
+                .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.message", Matchers.equalTo(mensagemErroEsperada)));
 
         Mockito.verify(criarUsuarioUseCase, times(1)).execute(Mockito.argThat(cmd ->
                 Objects.equals(nomeEsperado, cmd.getNome())
