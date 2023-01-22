1672ba7981acd7b3c1cc9d4523653ac33f639fb2
Lucas Reis <lucaschristian10@gmail.com>
2023-01-22 18:14:50 -0300

    feat(usuario): criar caso de uso obter usuario por id

--- src/main/java/com/example/base/application/usuario/recuperar/obter/ObterUsuarioPorIDUseCase.java
+++ src/main/java/com/example/base/application/usuario/recuperar/obter/ObterUsuarioPorIDUseCase.java
@@ -1,10 +1,28 @@
 package com.example.base.application.usuario.recuperar.obter;
 
 import com.example.base.application.UseCase;
+import com.example.base.application.usuario.exception.NotFoundException;
+import com.example.base.domain.usuario.Usuario;
+import com.example.base.infrastructure.usuario.persistence.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+
+@Component
 public class ObterUsuarioPorIDUseCase extends UseCase<String, ObterUsuarioPorIDOutput> {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Autowired
+    public ObterUsuarioPorIDUseCase(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
     @Override
-    public ObterUsuarioPorIDOutput execute(String s) {
-        return null;
+    public ObterUsuarioPorIDOutput execute(String input) {
+        return usuarioRepository
+                .findById(input)
+                .map(ObterUsuarioPorIDOutput::from)
+                .orElseThrow(() -> NotFoundException.with(Usuario.class, input));
     }
 }
