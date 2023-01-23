c5bf90f4486de88b7c1659bca966c43df7740d0f
Lucas Reis <lucaschristian10@gmail.com>
2023-01-23 14:19:09 -0300

    test(usuario): criar configuracoes necessarias para realziar testes e2e

--- src/test/java/com/example/base/e2e/usuario/UsuarioE2ETest.java
+++ src/test/java/com/example/base/e2e/usuario/UsuarioE2ETest.java
@@ -1,19 +1,11 @@
 package com.example.base.e2e.usuario;
 
 
-import com.example.base.application.usuario.criar.CriarUsuarioUseCase;
 import com.example.base.infrastructure.E2ETest;
-import com.example.base.infrastructure.security.JwtAuthenticationFilter;
-import com.example.base.infrastructure.security.JwtService;
-import com.example.base.infrastructure.usuario.persistence.UsuarioRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -24,18 +16,6 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers
 public class UsuarioE2ETest {
 
-    @Autowired
-    private MockMvc mvc;
-    @Autowired
-    private ObjectMapper mapper;
-    @Autowired
-    private JwtAuthenticationFilter authFilter;
-    @Autowired
-    private JwtService jwtService;
-    @MockBean
-    private UsuarioRepository usuarioRepository;
-    @MockBean
-    private CriarUsuarioUseCase criarUsuarioUseCase;
 
     @Container
     private static final MySQLContainer MY_SQL_CONTAINER =
